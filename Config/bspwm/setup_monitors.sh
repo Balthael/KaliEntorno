303523d3e8bf6ad5aad391a2e773f8bde56b54eb
balthael <balthael@balthael.balthael>
2025-04-29 22:52:16 -0500

    Picom Actualizada

--- Config/bspwm/setup_monitors.sh
+++ Config/bspwm/setup_monitors.sh
@@ -2,12 +2,10 @@
 
 # Apagar portátil y configurar disposición
 xrandr \
-  --output eDP-1 --off \
-  --output DP-1-3-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
-  --output DP-1-1         --mode 1920x1080 --pos 1920x0 --rotate normal
+  --output DP-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+  --output HDMI-0         --mode 1920x1080 --pos 1920x0 --rotate normal
 # Asignar escritorios si los nombres coinciden
-bspc monitor DP-1-3-1 -d I II III IV V
-bspc monitor DP-1-1   -d VI VII VIII IX X
+bspc monitor DP-2 -d I II III IV V VI VII VIII IX X
 
 bspc config remove_disabled_monitors true
 bspc config remove_unplugged_monitors true
